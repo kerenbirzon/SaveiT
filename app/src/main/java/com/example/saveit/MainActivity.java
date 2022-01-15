7fbe8b00ca50f5c6a5d53dcbcadea9dbfdfdca43
Tamir Amar <tamiramar22@cs.colman.ac.il>
2022-01-15 17:10:26 +0200

    config menu toolbar

--- app/src/main/java/com/example/saveit/MainActivity.java
+++ app/src/main/java/com/example/saveit/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.saveit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.NavHost;
@@ -8,6 +9,8 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     NavController navCtl;
@@ -23,5 +26,26 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this,navCtl);
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.basic_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if  (!super.onOptionsItemSelected(item)) {
+            switch (item.getItemId()){
+                case android.R.id.home:
+                    navCtl.navigateUp();
+                    return true;
+            }
+        }else {
+            return true;
+        }
+        return false;
     }
 }
