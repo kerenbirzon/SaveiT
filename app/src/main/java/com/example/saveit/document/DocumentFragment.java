932b3d93107bbacca98492d99bb03315443930e8
keren-birzon <keren.birzon@taranis.ag>
2022-01-16 21:58:49 +0200

    set add category layout

--- app/src/main/java/com/example/saveit/document/DocumentFragment.java
+++ app/src/main/java/com/example/saveit/document/DocumentFragment.java
@@ -18,6 +18,7 @@ public class DocumentFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_document, container, false);
         Button saveDocumentBtn = view.findViewById(R.id.btn_save_document);
