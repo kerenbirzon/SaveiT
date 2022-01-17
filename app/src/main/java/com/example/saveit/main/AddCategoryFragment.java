004f8ea3c358b0242b875a92901cd57e180babe3
keren-birzon <keren.birzon@taranis.ag>
2022-01-17 23:45:51 +0200

    chooseIcon fragment - select icon

--- app/src/main/java/com/example/saveit/main/AddCategoryFragment.java
+++ app/src/main/java/com/example/saveit/main/AddCategoryFragment.java
@@ -1,43 +1,73 @@
 package com.example.saveit.main;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import com.example.saveit.R;
+import com.example.saveit.model.Category;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class AddCategoryFragment extends Fragment {
+    public static final int DEFAULT_ICON = 13; //R.drawable.buy
+    private int iconImageValue = DEFAULT_ICON; //initialized to default icon
+    private static final int[] iconImages = {R.drawable.money, R.drawable.tax, R.drawable.lipstick, R.drawable.id, R.drawable.house, R.drawable.garden, R.drawable.fish, R.drawable.fan, R.drawable.email, R.drawable.dog, R.drawable.car, R.drawable.cake, R.drawable.buy, R.drawable.cat, R.drawable.company};
+    private String[] categoriesTitles; //categories titles not used for spinner
 
+    //widgets
+    private TextInputLayout titleInput;
+    private Button saveNewCategoryBtn, cancelNewCategoryBtn, chooseIconBtn;
+    private Spinner titleSpinner;
+    private ImageView iconPrevView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_category, container, false);
-        Button saveNewCategoryBtn = view.findViewById(R.id.btn_action_save);
+        saveNewCategoryBtn = view.findViewById(R.id.btn_action_save);
+        cancelNewCategoryBtn = view.findViewById(R.id.btn_action_cancel);
+        chooseIconBtn = view.findViewById(R.id.btn_choose_icon);
+        iconPrevView = view.findViewById(R.id.iv_icon_img_prev);
+        titleInput = view.findViewById(R.id.et_title);
+        titleSpinner = view.findViewById(R.id.spinner_title);
+        titleInput.setVisibility(View.INVISIBLE);
+
+
         saveNewCategoryBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //save the category to the model
+                Log.d("clicked", "save new category was clicked");
+                //if (userInputValid()) {
+                //    addNewCategory();
+                //} else {
+                //    Toast.makeText(getContext(), "invalid title", Toast.LENGTH_LONG).show();
+                //}
 
-                //pop back to category list
-                //Navigation.findNavController(view).navigate(R.id.action_addCategoryFragment_pop);
+                //Navigation.findNavController(view).navigate(CategoryListFragmentDirections.actionCategoryListToCategory(category.getTitle()));
+
+
             }
         });
-        Button cancelNewCategoryBtn = view.findViewById(R.id.btn_action_cancel);
         cancelNewCategoryBtn.setOnClickListener((v) -> {
+            Log.d("AddCategoryFragment", "onClick: closing AddCategoryFragment");
             Navigation.findNavController(v).navigateUp();
         });
-        //cancelNewCategoryBtn.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_addCategoryFragment_pop));
 
-        Button chooseIconBtn = view.findViewById(R.id.btn_choose_icon);
         chooseIconBtn.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_addCategory_to_chooseIcon));
 
         return view;
     }
+
 }
--- app/src/main/java/com/example/saveit/main/ChooseIconFragment.java
+++ app/src/main/java/com/example/saveit/main/ChooseIconFragment.java
@@ -1,5 +1,6 @@
 package com.example.saveit.main;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
@@ -11,11 +12,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.ImageView;
 
 import com.example.saveit.R;
 
 public class ChooseIconFragment extends Fragment {
     private Drawable highlight;
+    private int lastImageToBeSelected = 0;
+    private ImageView lastImage = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,18 +29,41 @@ public class ChooseIconFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_choose_icon, container, false);
         highlight = getResources().getDrawable(R.drawable.icon_selection);
         //setButtonsOnClickMethods(view);
+
+        GridLayout grid = view.findViewById(R.id.gl_choose_icon);
+        int childCount = grid.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            final ImageView currentImage = (ImageView) grid.getChildAt(i);
+            final int finalI = i;
+            currentImage.setOnClickListener(new ImageView.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (lastImage != null) {
+                        lastImage.setBackground(null);
+                    }
+                    lastImage = currentImage;
+                    lastImageToBeSelected = finalI;
+                    currentImage.setBackground(highlight);
+                }
+            });
+        }
+
+        Button cancelIconBtn = view.findViewById(R.id.btn_action_cancel);
         Button saveIconBtn = view.findViewById(R.id.btn_save_category_icon);
+
         saveIconBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                //save the category to the model
+            public void onClick(View v) {
+                Log.d("clicked", "actionSaveButton");
 
-                //pop back to category list
-                //Navigation.findNavController(view).navigate(R.id.action_chooseIconFragment_pop);
+                //Intent intent = new Intent(ChooseIconFragment.this.getContext(),AddCategoryFragment.class);
+                //intent.putExtra("iconIntValue", lastImageToBeSelected);
+
+                //Navigation.findNavController(view).navigate(ChooseIconFragmentDirections.actionChooseIconFragmentPop());
+                // need to continue here!!!!!!
             }
         });
 
-        Button cancelIconBtn = view.findViewById(R.id.btn_action_cancel_icon_selection);
         cancelIconBtn.setOnClickListener((v) -> {
             Navigation.findNavController(v).navigateUp();
         });
@@ -43,5 +71,4 @@ public class ChooseIconFragment extends Fragment {
         return view;
     }
 
-//    private void setButtonsOnClickMethods(View view) {    }
 }
