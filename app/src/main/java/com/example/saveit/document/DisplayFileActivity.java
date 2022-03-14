a93ba937f35847b4caaadff3bb4ff3f62e22edff
keren-birzon <keren.birzon@taranis.ag>
2022-03-15 00:09:16 +0200

    display file activity and layout

---
+++ app/src/main/java/com/example/saveit/document/DisplayFileActivity.java
@@ -0,0 +1,65 @@
+package com.example.saveit.document;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.saveit.R;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+/**
+ * a class that displays a document file
+ */
+public class DisplayFileActivity extends AppCompatActivity {
+
+    public static final String GOOGLE_PREFIX_URL = "https://drive.google.com/viewerng/viewer?embedded=true&url=";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_file);
+        Toolbar mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        final ActionBar actionBar = getSupportActionBar();
+
+        final Intent intentCreatedMe = getIntent();
+        final String file = intentCreatedMe.getStringExtra("file_url");
+        String url = null;
+        try {
+            url = URLEncoder.encode(file, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        String doc = GOOGLE_PREFIX_URL + url;
+        final WebView webView = (WebView) findViewById(R.id.webView);
+        webView.setVisibility(View.INVISIBLE);
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                actionBar.setTitle("Loading..");
+                if (newProgress == 100) {
+                    progressBar.setVisibility(View.GONE);
+                    webView.setVisibility(View.VISIBLE);
+                    actionBar.setTitle("SaveMe");
+                }
+            }
+        });
+        webView.loadUrl(doc);
+    }
+}
