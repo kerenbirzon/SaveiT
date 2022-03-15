8ba9cf3094732d299fa8a80fa1edc332f7edc9c2
keren-birzon <keren.birzon@taranis.ag>
2022-03-15 23:02:38 +0200

    basic functionality, getting the documentlist from firebase

--- app/src/main/java/com/example/saveit/category/CategoryActivity.java
+++ app/src/main/java/com/example/saveit/category/CategoryActivity.java
@@ -2,14 +2,50 @@ package com.example.saveit.category;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import com.example.saveit.R;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class CategoryActivity extends AppCompatActivity {
+    private ArrayList<Document> documentList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+        Intent mainIntent = getIntent();
+        Gson gson = new Gson();
+        String json = mainIntent.getStringExtra("docList");
+        Type type = new TypeToken<ArrayList<Document>>() {
+        }.getType();
+        documentList = gson.fromJson(json, type);
+        if (documentList == null) {
+            documentList = new ArrayList<>();
+        }
+
+        // initializes the recycler view and the adapter
+        initializeRecyclerView();
+
+        // when a document is clicked
+        initializeDocumentClickListener();
+
+        // when a document is long clicked
+        initializeDocumentLongClickListener();
+
+
+    }
+
+    private void initializeRecyclerView() {
+    }
+    private void initializeDocumentClickListener() {
+    }
+    private void initializeDocumentLongClickListener() {
     }
 }
