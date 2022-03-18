8637ac6b05a1591d7f539fd9fbbc10f97e741f43
keren-birzon <keren.birzon@taranis.ag>
2022-03-18 23:34:08 +0200

    save and cancek button in add category

--- app/src/main/java/com/example/saveit/main/AddCategoryDialog.java
+++ app/src/main/java/com/example/saveit/main/AddCategoryDialog.java
@@ -9,27 +9,44 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import com.example.saveit.R;
 import com.example.saveit.model.CategoryModel;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class AddCategoryDialog extends Fragment {
     public static final int DEFAULT_ICON = 13; //R.drawable.buy
+    private static final int CATEGORY_ICON_REQUEST_CODE = 111;
     private int iconImageValue = DEFAULT_ICON; //initialized to default icon
     private static final int[] iconImages = {R.drawable.money, R.drawable.tax, R.drawable.lipstick, R.drawable.id, R.drawable.house, R.drawable.garden, R.drawable.fish, R.drawable.fan, R.drawable.email, R.drawable.dog, R.drawable.car, R.drawable.cake, R.drawable.buy, R.drawable.cat, R.drawable.company};
     private String[] categoriesTitles; //categories titles not used for spinner
     private boolean isCategoryTitleValid = false;
     public static final int MAX_CATEGORY_NAME_LENGTH = 20;
+    private static final String TAG = "AddCategoryFragment";
 
+    public AddCategoryDialog() {
+    }
 
+    public AddCategoryDialog(String[] categoriesTitles) {
+        this.categoriesTitles = categoriesTitles;
+    }
 
-    private EditText categoryNameEt;
+    public interface OnInputListener {
+        void sendInput(String title, int image);
+    }
+
+    public OnInputListener mOnInputListener;
+    //widgets
+    private TextInputLayout titleInput;
     private Button saveNewCategoryBtn, cancelNewCategoryBtn, chooseIconBtn;
+    private Spinner titleSpinner;
     private ImageView iconPrevView;
-    private ProgressBar progressBar;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,21 +56,30 @@ public class AddCategoryDialog extends Fragment {
         cancelNewCategoryBtn = view.findViewById(R.id.btn_action_cancel);
         chooseIconBtn = view.findViewById(R.id.btn_choose_icon);
         iconPrevView = view.findViewById(R.id.iv_icon_img_prev);
+        titleInput = view.findViewById(R.id.et_title);
+        titleInput.setVisibility(View.INVISIBLE);
+        titleSpinner = view.findViewById(R.id.spinner_title);
+        setCategoryTitle();
 
-
-
+        cancelNewCategoryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: closing dialog");
+                getDialog().dismiss();
+            }
+        });
         saveNewCategoryBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Log.d("clicked", "save new category was clicked");
-                saveCategory();
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: capturing input");
+                if (userInputValid()) {
+                    addNewCategory();
+                } else {
+                    Toast.makeText(getContext(), "invalid title", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
-//        cancelNewCategoryBtn.setOnClickListener((v) -> {
-//            Log.d("AddCategoryFragment", "onClick: closing AddCategoryFragment");
-//            Navigation.findNavController(v).navigateUp();
-//        });
 
         chooseIconBtn.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_addCategory_to_chooseIcon));
 
