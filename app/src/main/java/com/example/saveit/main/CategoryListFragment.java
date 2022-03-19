c4e7d9c8d27c20c478ae76261eaea262d5b13113
Tamir Amar <tamir.amar@zerto.com>
2022-03-19 19:16:23 +0200

    fixing auto reload for categories

--- app/src/main/java/com/example/saveit/main/CategoryListFragment.java
+++ app/src/main/java/com/example/saveit/main/CategoryListFragment.java
@@ -3,15 +3,22 @@ package com.example.saveit.main;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.os.HandlerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
@@ -22,9 +29,15 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import com.example.saveit.MainActivity;
 import com.example.saveit.R;
 import com.example.saveit.login.LoginActivity;
+import com.example.saveit.model.AppLocalDb;
 import com.example.saveit.model.Category;
 import com.example.saveit.model.CategoryModel;
 import com.google.firebase.auth.FirebaseAuth;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class CategoryListFragment extends Fragment {
     CategoryListViewModel viewModel;
@@ -34,6 +47,9 @@ public class CategoryListFragment extends Fragment {
     FirebaseAuth categoryListmAuth;
     Button addCategoryBtn,signOutBtn;
     ImageView iconPrevView;
+
+    public Executor executor = Executors.newFixedThreadPool(1);
+    public Handler mainThread = HandlerCompat.createAsync(Looper.getMainLooper());
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -59,18 +75,31 @@ public class CategoryListFragment extends Fragment {
 
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        categoryAdapter = new CategoryAdapter(viewModel.getCategories().getValue());
+        categoryAdapter = new CategoryAdapter();//viewModel.getCategories().getValue()
         recyclerView.setAdapter(categoryAdapter);
 
-        categoryAdapter.setCategoryClickListener(new CategoryClickListener() {
+
+        categoryAdapter.setOnItemClickListener(new OnItemClickListener() {
             @Override
-            public void onCategoryClicked(View v,int position) {
-                Log.d("category clicked", "category was clicked");
-                lastCategoryPosition = position;
-                Category category = viewModel.getCategories().getValue().get(position);
-                //Navigation.findNavController(view).navigate(CategoryListFragmentDirections.actionCategoryListToCategory(category.getTitle()));
+            public void onItemClick(View v,int position) {
+                String categoryTitle = viewModel.getCategories().getValue().get(position).getTitle();
+                Navigation.findNavController(v).navigate(CategoryListFragmentDirections.actionCategoryListToCategory(categoryTitle));
+
             }
         });
+
+//        categoryAdapter.setOnItemClickListener(new OnItemClickListener() { //setCategoryClickListener CategoryClickListener
+//            @Override
+//            public void onCategoryClicked(View v,int position) {
+//                Log.d("category clicked", "category was clicked");
+//                lastCategoryPosition = position;
+//                Category category = viewModel.getCategories().getValue().get(position);
+//                Navigation.findNavController(v).navigate(CategoryListFragmentDirections.actionCategoryListToCategory(category.getTitle()));
+//
+//            }
+//        });
+
+        setHasOptionsMenu(true);
 
         viewModel.getCategories().observe(getViewLifecycleOwner(), categories -> refresh());
         swipeRefresh.setRefreshing(CategoryModel.instance.getCategoryListLoadingState().getValue() == CategoryModel.CategoryListLoadingState.loading);
@@ -96,6 +125,79 @@ public class CategoryListFragment extends Fragment {
 
     private void refresh() {
         categoryAdapter.notifyDataSetChanged();
-        swipeRefresh.setRefreshing(false);
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView title;
+        ImageView image;
+
+        public MyViewHolder(@NonNull View itemView, OnItemClickListener listener) {
+            super(itemView);
+            title = itemView.findViewById(R.id.category_title);
+            image = itemView.findViewById(R.id.category_img);
+
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = getAdapterPosition();
+                    listener.onItemClick(v,pos);
+                }
+            });
+        }
+
+        void bind(Category category){
+            title.setText(category.getTitle());
+            image.setImageResource(Integer.parseInt(category.getImage()));
+        }
+    }
+
+    interface OnItemClickListener{
+        void onItemClick(View v,int position);
+    }
+    class CategoryAdapter extends RecyclerView.Adapter<MyViewHolder>{
+
+        OnItemClickListener listener;
+        public void setOnItemClickListener(OnItemClickListener listener){
+            this.listener = listener;
+        }
+
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(R.layout.category_item,parent,false);
+            MyViewHolder holder = new MyViewHolder(view,listener);
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+            Category category = viewModel.getCategories().getValue().get(position);
+            holder.bind(category);
+        }
+
+        @Override
+        public int getItemCount() {
+            if(viewModel.getCategories().getValue() == null){
+                return 0;
+            }
+            return viewModel.getCategories().getValue().size();
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.basic_menu,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.addCategoryFragment){
+            Log.d("TAG","ADD...");
+            return true;
+        }else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
